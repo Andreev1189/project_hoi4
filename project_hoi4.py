0317651e00fddffb5726523e42e2db2f57131ab9
Блохин Олег <blokhin.oi@phystech.edu>
2021-12-15 20:06:03 +0300

    added new classes

--- project_hoi4.py
+++ project_hoi4.py
@@ -9,6 +9,7 @@ pygame.init()
 FPS = 30
 TIME = 0
 TIMESPEED = 10
+
 BORDERS = (1200, 900)
 screen = pygame.display.set_mode(BORDERS)
 
@@ -16,21 +17,47 @@ provinces = [[0, 100, 100], [1, 200, 100], [2, 150, 100 * (1 + 0.5 * sqrt(3))]]
 ways = [[1, 2], [2, 0], [0, 1]]
 
 
-# def find_way_to_np:
+
+def create_map():
+    prov = Province()
+    Provinces.append(prov)
+
 
 def graf_printer(p, w):
     for np in p:
         circle(screen, (0, 255, 0), (np[1], np[2]), 10, 10)
     for way in w:                                           # He y4uTblBaeT Henop9IgkoBble np.
-        polygon(screen, (255, 255, 0), [(p[way[0]][1], p[way[0]][2]), (p[way[1]][1], p[way[1]][2]), (p[way[0]][1], p[way[0]][2])], 5)
+        polygon(screen, (255, 255, 0), [(p[way[0]][1], p[way[0]][2]), (p[way[1]][1], p[way[1]][2]), 
+                                                                (p[way[0]][1], p[way[0]][2])], 5)
 
 class Division:
-    def __init__(self):
-        number = 0
-        v = 1
+    def __init__(self, current_prov, chosen=False, velocity = 1, r = 10, alive = 0):
+        self.number = number
+        self.current_prov = number
+        self.velocity = 1
+        self.chosen = False
+        self.r = 10
 
     def move(self):
         pass
+
+    def draw(self):
+        pass
+
+    def chosen(self, event):
+        pass
+
+
+
+
+class Province:
+    def __init__(self, x, y, motherland):
+        self.x = x
+        self.y = y 
+        self.motherland = motherland
+
+
+
 
 def printer(title, text_size=15, base_coords=(10, 10)):
     font = pygame.font.Font(None, text_size)
@@ -60,9 +87,11 @@ while not finished:
     printer(TIME)
     print(TIME)
 
+    ()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+
 
     graf_printer(provinces, ways)
     pygame.display.update()
