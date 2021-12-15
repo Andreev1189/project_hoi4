0db1b785f6b8b6be39981109b53dfbaddbdfca66
Блохин Олег <blokhin.oi@phystech.edu>
2021-12-16 00:12:21 +0300

    на сегодня хватит

--- project_hoi4.py
+++ project_hoi4.py
@@ -27,8 +27,11 @@ provinces = [[0, 100, 100], [1, 200, 100], [2, 150, 100 * (1 + 0.5 * sqrt(3))]]
 ways = [[1, 2], [2, 0], [0, 1]]
 
 K = []
+Z = []
 Provinces = []
 Divisions = []
+Lines = []
+
 
 def create_map(el):
 
@@ -37,18 +40,25 @@ def create_map(el):
     print(int(el[1]))
     Provinces.append(prov)
 
+def craeate_lines(el):
+
+    line = Way(int(el[0]), int(el[1]))
+    Lines.append(line)
+    print(int(el[0]))
+
+
 def create_division(current_prov, color):
     div = Division(current_prov, color)
     Divisions.append(div)
 
-
+'''
 def graf_printer(p, w):
     for np in p:
         circle(screen, (0, 255, 0), (np[1], np[2]), 10, 10)
     for way in w:                                           # He y4uTblBaeT Henop9IgkoBble np.
         polygon(screen, (255, 255, 0), [(p[way[0]][1], p[way[0]][2]), (p[way[1]][1], p[way[1]][2]), 
                                                                 (p[way[0]][1], p[way[0]][2])], 5)
-
+'''
 
 def printer(title, text_size=15, base_coords=(10, 10)):
     font = pygame.font.Font(None, text_size)
@@ -57,28 +67,36 @@ def printer(title, text_size=15, base_coords=(10, 10)):
 
 
 class Division:
-    def __init__(self, current_prov, color, chosen=False, velocity=1, r=10, alive=1):
+    def __init__(self, current_prov, color, chosen=False, current_way=-1, way_completed=-1, velocity=1, r=10, alive=1):
         
         self.current_prov = current_prov
         self.color = color
         self.velocity = 1
         self.chosen = False
+        self.current_way = current_way
+        self.way_completed = way_completed
         self.r = 10
         self.alive = alive
+
+    def draw(self, Provinces):
+        pygame.draw.rect(screen, self.color, 
+            (Provinces[self.current_prov].x - self.r, Provinces[self.current_prov].y - 0.5 * self.r,
+                2 * self.r, self.r))
 
     def move(self):
         pass
 
-    def draw(self, Provinces):
-        pygame.draw.rect(screen, self.color, 
-            (Provinces[self.current_prov].x - self.r, Provinces[self.current_prov].y - self.r,
-                2 * self.r, self.r))
-
     def chosen(self, event):
         pass
 
+    def chosen_up(self):
+        self.chosen = True
+
+    def chosen_down(self):
+        self.chosen = False
+
 class Province:
-    def __init__(self, x, y, motherland, color=GREEN, r=20):
+    def __init__(self, x, y, motherland, color=GREEN, r=15):
         self.x = x
         self.y = y 
         self.motherland = motherland
@@ -89,24 +107,39 @@ class Province:
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
 class Way:
-    def __init__(self, start_pos, end_pos):
+    def __init__(self, start_pos, end_pos, color=YELLOW):
         self.start_pos = start_pos
         self.end_pos = end_pos
+        self.color = color
 
-    def draw(self, Provinces)
-        pass
+    def draw(self, Provinces):
+        pygame.draw.line(screen, BLUE, (Provinces[self.start_pos].x, Provinces[self.start_pos].y), 
+                                        (Provinces[self.end_pos].x, Provinces[self.end_pos].y), 10)
 
-f = open('map.txt', 'r')
-for line in f:
+
+f1 = open('map.txt', 'r')
+f2 = open('ways.txt', 'r')
+
+for line in f1:
     line = line.rstrip()
     K.append(line)
 
     print(line)
     print(type(line))
 
+for line in f2:
+    line = line.rstrip()
+    Z.append(line)
+
+
 for i, el in enumerate(K):
-    el = el.split()
+    el = el.split(' ')
     create_map(el)
+
+for i, el in enumerate(Z):
+    el = el.split()
+    craeate_lines(el)
+
 
 create_division(0, MAGENTA)
 
@@ -123,20 +156,25 @@ finished = False
 while not finished:
     clock.tick(FPS)
 
+    
     TIME = timeboss(TIME)
     printer(TIME)
+    
+
+    for i, el in enumerate(Lines):
+        el.draw(Provinces)
+
+    for i, el in enumerate(Provinces):
+        el.draw()
+
     Divisions[0].draw(Provinces)
- 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
 
-
-    graf_printer(provinces, ways)
+        
     pygame.display.update()
     screen.fill((0, 0, 0))
-
-
 
 pygame.quit()
